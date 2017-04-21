02b8843a060dd9720e18daf851926b3280d7c0b0
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-04-22 00:31:38 +0100

    add integration check force picking up local package update readme to correct config publish command

--- ci/scripts/integration-test.sh
+++ ci/scripts/integration-test.sh
@@ -4,4 +4,7 @@ php -S localhost:8000 server.php &
 sleep 10
 result=$(curl http://localhost:8000)
 pkill -9 php
-echo $result
+if [[ "$result" == *"nb_uniq_visitors"* ]]; then
+    exit 0
+fi
+exit 1
