1c87f425bcb4fb8665c95964c7eda6295d9b3380
Rob Brazier <rob@brzr.co>
2017-04-22 10:05:19 +0100

    increase debugging on integration and increase test timeout

--- ci/scripts/laravel.sh
+++ ci/scripts/laravel.sh
@@ -2,9 +2,9 @@
 
 composer create-project laravel/laravel integration "${LARAVEL_VERSION}.*" --prefer-source
 
-cd plugin
-git config --global user.email "php@docker"
-git config --global user.name "Docker Container"
-git init
-git add --all .
-git commit -m "Initial commit"
+#cd plugin
+#git config --global user.email "php@docker"
+#git config --global user.name "Docker Container"
+#git init
+#git add --all .
+#git commit -m "Initial commit"
