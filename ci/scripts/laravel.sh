26c9498f8fee3683f77f6d931a66134a892a38fb
Rob Brazier <rob@brzr.co>
2017-04-25 20:53:25 +0100

    remove dev dependencies for integration

--- ci/scripts/laravel.sh
+++ ci/scripts/laravel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-composer create-project laravel/laravel integration "${LARAVEL_VERSION}.*" --no-progress
+composer create-project laravel/laravel integration "${LARAVEL_VERSION}.*" --no-progress --no-dev
