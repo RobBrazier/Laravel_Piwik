2dee2860a5344dbb2abc6b799a75404673d6cd2f
Rob Brazier <rob@brzr.co>
2017-04-25 21:23:56 +0100

    add prestissimo for faster composer installs

--- ci/scripts/setup.sh
+++ ci/scripts/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 apk add composer bash jq --update-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/
+composer global require hirak/prestissimo
 chown -R www-data:www-data .
