5bccac8a8f80648d6d426a2ed13975c46b95ca81
Rob Brazier <rob@brzr.co>
2017-04-29 19:38:38 +0100

    update ci scripts and integration config

--- ci/scripts/laravel.sh
+++ ci/scripts/laravel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-composer global require hirak/prestissimo
+composer global require hirak/prestissimo --no-progress
 composer create-project laravel/laravel integration "${LARAVEL_VERSION}.*" --no-progress --no-dev
