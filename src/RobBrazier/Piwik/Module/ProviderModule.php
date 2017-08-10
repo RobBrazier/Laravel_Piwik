f99a7e4f11d24cf9f26c5e01458ec6930e5500a4
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-08-10 19:35:49 +0100

    remove unnecessary whitespace

--- src/RobBrazier/Piwik/Module/ProviderModule.php
+++ src/RobBrazier/Piwik/Module/ProviderModule.php
@@ -8,7 +8,7 @@ namespace RobBrazier\Piwik\Module;
  * @see https://developer.piwik.org/api-reference/reporting-api#Provider for arguments
  */
 class ProviderModule extends Module {
-  
+
     /**
      * @param array[string]mixed $arguments extra arguments to be passed to the api call
      * @param string $format override format (defaults to one specified in config file)
