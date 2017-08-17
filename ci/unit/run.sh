6a37db791f58647b53f503e31a78759b44f83729
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-08-17 19:22:44 +0100

    run scripts as www-data

--- ci/unit/run.sh
+++ ci/unit/run.sh
@@ -3,5 +3,5 @@
 app_dir="/usr/src/app"
 scripts_dir="$app_dir/ci/scripts"
 sh "$scripts_dir/setup.sh"
-bash "$scripts_dir/install.sh"
-bash "$scripts_dir/test.sh"
+sudo -u www-data -H bash "$scripts_dir/install.sh"
+sudo -u www-data -H bash "$scripts_dir/test.sh"
