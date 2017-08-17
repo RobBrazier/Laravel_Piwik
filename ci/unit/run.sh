e3f74e9b9542227c99ea0122286ed759a3782a94
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-08-17 19:17:58 +0100

    update unit run.sh

--- ci/unit/run.sh
+++ ci/unit/run.sh
@@ -2,6 +2,6 @@
 
 app_dir="/usr/src/app"
 scripts_dir="$app_dir/ci/scripts"
-exec "$scripts_dir/setup.sh"
-exec "$scripts_dir/install.sh"
-exec "$scripts_dir/test.sh"
+sh "$scripts_dir/setup.sh"
+bash "$scripts_dir/install.sh"
+bash "$scripts_dir/test.sh"
