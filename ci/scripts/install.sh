3091f80d1a416c1063a6274afd3f9b9d638856f2
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-08-17 19:27:06 +0100

    fix killed install script

--- ci/scripts/install.sh
+++ ci/scripts/install.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-composer global require hirak/prestissimo --no-progress
-composer install --no-suggest --no-progress
+composer global require hirak/prestissimo --no-progress && composer install --no-suggest --no-progress
