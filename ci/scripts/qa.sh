98918c75cdddb89c591deb45072fc289fb186952
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-08-18 17:21:56 +0100

    consolidate qa scripts into one image

---
+++ ci/scripts/qa.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+export APP_DIR="/usr/src/app"
+export SCRIPTS_DIR="$APP_DIR/ci/scripts"
+sh "$SCRIPTS_DIR/setup.sh"
+
+runScript() {
+  sudo -E -u www-data -H bash $*
+}
+
+runScript "$SCRIPTS_DIR/install.sh"
+apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community php7-xdebug
+docker-php-ext-enable /usr/lib/php7/modules/xdebug.so
+composer run-script test
+ls -la reports/tests.xml
