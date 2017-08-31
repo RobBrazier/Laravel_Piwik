01995cde1a4e12a60e16e2307155b32fa962fa80
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-08-31 22:35:42 +0100

    fix init

--- ci/init/run.sh
+++ ci/init/run.sh
@@ -3,4 +3,4 @@
 export APP_DIR="/usr/src/app"
 export SCRIPTS_DIR="$APP_DIR/ci/scripts"
 sh "$SCRIPTS_DIR/setup.sh"
-sudo -E -u www-data -H bash "$SCRIPTS_DIR/install.sh true true"
+sudo -E -u www-data -H bash "$SCRIPTS_DIR/install.sh" true true
