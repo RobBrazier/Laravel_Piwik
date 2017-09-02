82e2447a08595b62a9bfa3c47b0dc7fbbff5db8b
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-09-02 14:41:49 +0100

    switch to composer sami version

--- ci/docs/sami.sh
+++ ci/docs/sami.sh
@@ -5,6 +5,8 @@ set -x
 export APP_DIR="/usr/src/app"
 export SCRIPTS_DIR="$APP_DIR/ci/scripts"
 
+sami="/home/www-data/.composer/vendor/bin/sami"
+
 runScript() {
   sudo -E -u www-data -H $*
 }
@@ -13,7 +15,7 @@ sh "$SCRIPTS_DIR/setup.sh"
 runScript "bash $SCRIPTS_DIR/install.sh" false false
 chown -R www-data:www-data ..
 apk add --no-cache git
-runScript curl -O http://get.sensiolabs.org/sami.phar
-runScript php sami.phar update .sami.php
+runScript composer global require "sami/sami:4.0.*"
+runScript "$sami update .sami.php"
 runScript mv ../docs ./build
 runScript ls -la build
