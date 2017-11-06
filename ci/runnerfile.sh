5571763873c30cc30c7d0b53b5cb209ed682d7b8
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-11-06 20:56:14 +0000

    ditch sonarqube due to unreliability of sonarcloud.io

--- ci/runnerfile.sh
+++ ci/runnerfile.sh
@@ -31,7 +31,7 @@ task_integrationTest() {
 task_qa() {
   phpVersion="7.2"
   containerName="$containerNamePrefix-qa-${phpVersion/\./-}"
-  run_container_with_snapshot_volume $containerName $appDir $appDir "robbrazier/php:$phpVersion" "./ci/qa.sh" "BRANCH_NAME,PULL_REQUEST_NUMBER,SEMAPHORE_REPO_SLUG,SONAR_TOKEN,GITHUB_TOKEN"
+  run_container_with_snapshot_volume $containerName $appDir $appDir "robbrazier/php:$phpVersion" "./ci/qa.sh" "CODACY_PROJECT_TOKEN"
 }
 
 task_publish_docs() {
