4b92fd1eb852a97868dbab66c24812c03f4102db
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-12-09 20:52:16 +0000

    code style fixes

--- src/RobBrazier/Piwik/Repository/ConfigRepository.php
+++ src/RobBrazier/Piwik/Repository/ConfigRepository.php
@@ -7,7 +7,7 @@ interface ConfigRepository
     /**
      * Retrieve a configuration item.
      *
-     * @param string $key     configuration key
+     * @param string     $key     configuration key
      * @param mixed|null $default default value
      *
      * @return mixed
