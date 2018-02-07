38d35d0d1b6e7d8f4eaf8a30b81eaba40a749b8c
Rob Brazier <RobBrazier@users.noreply.github.com>
2018-02-07 21:47:00 +0000

    add qa task

--- ci/qa.sh
+++ ci/qa.sh
@@ -1,24 +1,19 @@
 #!/bin/sh
 set -e -x
-export APP_DIR="/usr/src/app"
-export SCRIPTS_DIR="$APP_DIR/ci/scripts"
-. "$SCRIPTS_DIR/common.sh"
+export SCRIPTS_DIR="ci/scripts"
 
+reporter="/tmp/cc-test-reporter"
+sh "$SCRIPTS_DIR/setup.sh"
+apk add --no-cache curl git
+curl -Lo $reporter https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64
+chmod +x $reporter
+$reporter before-build
+exit_code="0"
 {
-  reporter="/tmp/cc-test-reporter"
-  sh "$SCRIPTS_DIR/setup.sh"
-  apk add --no-cache curl git
-  runScript "curl -Lo $reporter https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64"
-  runScript "chmod +x $reporter"
-  runScript "$reporter before-build"
-  exit_code="0"
-  {
-    runScript "composer run-script coverage"
-    runScript "cp $APP_DIR/reports/coverage.xml $APP_DIR/clover.xml"
-  } || {
-    exit_code="$1"
-  }
-  runScript "$reporter after-build --exit-code $exit_code --coverage-input-type clover"
-  runScript "rm $APP_DIR/clover.xml"
-} || true
-chown -R "$CURRENT_USER:$CURRENT_GROUP" "$APP_DIR"
+  composer run-script coverage
+  cp reports/coverage.xml clover.xml
+} || {
+  exit_code="$1"
+}
+$reporter after-build --exit-code $exit_code --coverage-input-type clover
+rm clover.xml
--- ci/unit.sh
+++ ci/unit.sh
@@ -2,5 +2,4 @@
 set -e -x
 export SCRIPTS_DIR="ci/scripts"
 sh "$SCRIPTS_DIR/setup.sh"
-# bash $SCRIPTS_DIR/install.sh
 composer run-script test
