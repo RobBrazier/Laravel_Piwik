c4f8767e969b900e7e0c7b9523adefede6b70cca
Rob Brazier <RobBrazier@users.noreply.github.com>
2018-04-18 23:47:51 +0100

    Add composer install to pipeline

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -7,7 +7,9 @@ declare -a PHP_VERSIONS=(5.6 7.0 7.1 7.2)
 echo "steps:"
 for phpver in ${PHP_VERSIONS[@]}; do
     echo "  - label: ':php: $phpver Unit'"
-    echo "    command: .ci/unit.sh"
+    echo "    command:"
+    echo "      - composer install --no-suggest --no-progress"
+    echo "      - .ci/unit.sh"
     echo "    plugins:"
     echo "      docker-compose#v2.0.0:"
     echo "        run: php${phpver//\./}"
