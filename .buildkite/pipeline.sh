dde8b764c651e698c19396b0a6c3dc0aca1d98f3
Rob Brazier <RobBrazier@users.noreply.github.com>
2018-07-18 06:57:32 +0100

    Add smoke testing

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -3,6 +3,7 @@
 set -eu
 
 declare -a PHP_VERSIONS=(5.6 7.0 7.1 7.2)
+declare -a LARAVEL_VERSIONS=(5.1 5.2 5.3 5.4 5.5)
 
 echo "steps:"
 for phpver in ${PHP_VERSIONS[@]}; do
@@ -12,4 +13,13 @@ for phpver in ${PHP_VERSIONS[@]}; do
     echo "    plugins:"
     echo "      docker-compose#v2.5.0:"
     echo "        run: php${phpver//\./}"
+done
+echo "  - wait"
+for laravelver in ${LARAVEL_VERSIONS[@]}; do
+    echo "  - label: ':construction: Laravel $laravelver Smoke Test'"
+    echo "    command:"
+    echo "      - .ci/integration.sh"
+    echo "    plugins:"
+    echo "      docker-compose#v2.5.0:"
+    echo "        run php72"
 done
