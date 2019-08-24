f98c8848e6e991cf3cd252108e017607cb487cab
Rob Brazier <RobBrazier@users.noreply.github.com>
2019-08-24 12:51:46 +0100

    add unit tests and phpdocs to UsersManagerModule

--- src/RobBrazier/Piwik/Module/UsersManagerModule.php
+++ src/RobBrazier/Piwik/Module/UsersManagerModule.php
@@ -3,15 +3,18 @@
 namespace RobBrazier\Piwik\Module;
 
 /**
- * Class SitesManagerModule.
+ * Class UsersManagerModule.
  *
- * @see https://developer.matomo.org/api-reference/reporting-api#SitesManager for arguments
+ * @see https://developer.matomo.org/api-reference/reporting-api#UsersManager for arguments
  */
 class UsersManagerModule extends Module
 {
     /**
-     * @param string $group group to search for
-     * @param string $format override format (defaults to one specified in config file)
+     * Gets the user's Authentication Token
+     * 
+     * @param string $userLogin   user login
+     * @param string $md5Password md5 hashed password
+     * @param string $format      override format (defaults to one specified in config file)
      *
      * @return mixed
      */
@@ -29,6 +32,14 @@ class UsersManagerModule extends Module
         return $this->request->send($options);
     }
 
+    /**
+     * Assign an access role to a specified user for one or many site ids
+     * 
+     * @param string $userLogin  user login
+     * @param string $access     user access role ({@see https://matomo.org/docs/manage-users/#advanced-user-management})
+     * @param array $idSites     site ids to give the user access to
+     * @param string $format     override format (defaults to one specified in config file)
+     */
     public function setUserAccess($userLogin, $access, $idSites, $format = null)
     {
         {
@@ -47,15 +58,30 @@ class UsersManagerModule extends Module
 
     }
 
-    public function addUser($userLogin, $password, $email, $alias = '', $format = null)
+    /**
+     * Create a user
+      * 
+     * @param string $userLogin     user login
+     * @param string $password      user password
+     * @param string $email         user email
+     * @param string $alias         user alias (optional)
+     * @param string $initialIdSite initial site id to give access to
+     * @param string $format        override format (defaults to one specified in config file)
+     */
+    public function addUser($userLogin, $password, $email, $alias = null, $initialIdSite = null, $format = null)
     {
         {
             $arguments = [
                 'userLogin' => $userLogin,
                 'password' => $password,
                 'email' => $email,
-                'alias' => $alias,
             ];
+            if (isset($alias)) {
+                $arguments = array_add($arguments, 'alias', $alias);
+            }
+            if (isset($initialIdSite)) {
+                $arguments = array_add($arguments, 'initialIdSite', $initialIdSite);
+            }
             $options = $this->getOptions($format)
                 ->useSiteId(false)
                 ->usePeriod(false)
